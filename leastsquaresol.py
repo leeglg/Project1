d1303df438817dad97ffe3461f5571434cdcfb7e
LeeGeom <dl3025rja@gmail.com>
2022-02-24 17:41:46 +0900

    완성

--- leastsquaresol.py
+++ leastsquaresol.py
@@ -1,14 +1,24 @@
 # 1_3
 import numpy as np
 
+
+def nsel(m):
+    n = np.random.randint(1, 3)
+    if (n==m):
+        return nsel(m)
+    return n
+
 def leastsquaresol():
     # A가 정방행렬 아닐 때
     # A B 만들고
-    tmp1 = np.random.randint(1, 5)
-    tmp2 = np.random.randint(1, 5)
+    m = np.random.randint(1, 3) # A의 행 수 = B의 행 수
+    n = nsel(m)                 # A의 열 수 =/= m
 
-    A = np.random.randint(0, 10, size=(tmp1, tmp2))
-    B = np.random.randint(0, 10, size=(tmp2, 1))
+
+    tmp3 = np.random.randint(1, 3) # B의 열 수
+    A = np.random.randint(0, 10, size=(m, n))
+    B = np.random.randint(0, 10, size=(m, tmp3))
+
     print("A\n", A)
     print("B\n", B)
 
@@ -16,6 +26,6 @@ def leastsquaresol():
     Apinv = np.linalg.pinv(A)
     print("Ainv\n", Apinv)
 
-    # x = A^-1 * B
+    # x = A^-1 * B   근사값.
     x = np.einsum("ij,jk->ik", Apinv, B)
     print("x\n", x)
--- linearsol.py
+++ linearsol.py
@@ -4,6 +4,9 @@ import numpy as np
 def linearsol(n):
     # A가 정방행렬일 때
     # A B 만들고
+
+
+
     A = np.random.randint(0, 10, size=(n, n))
     B = np.random.randint(0, 10, size=(n, 1))
     print("A\n", A)
